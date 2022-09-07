ba51e4deb8b43da4f8a876e0ce827059daecae3e
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-09-07 15:58:48 -0400

    2.19: Phonebook step11

--- react/part2/phonebook/phonebook-app/src/App.js
+++ react/part2/phonebook/phonebook-app/src/App.js
@@ -3,12 +3,15 @@ import personsService from './services/persons'
 import Filter from './components/Filter'
 import PersonForm from "./components/PersonForm";
 import Phonebook from "./components/Phonebook";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
 
   useEffect(() => {
@@ -32,7 +35,6 @@ const App = () => {
     setFilterName(event.target.value)
   }
 
-
   const addName = (event) => {
     event.preventDefault()
     const personObject = {
@@ -40,6 +42,7 @@ const App = () => {
       number : newNumber
     }
 
+	//go through the persons array and find if there's a matching name already inside
     if(persons.some(el => el.name === newName)){
 		const personID = persons.filter(person => person.name === newName)
 		//if the name already exists, ask the user if they want to replace their number with the one provided in newName
@@ -50,6 +53,7 @@ const App = () => {
 			setPersons(persons.map(person => person.id === personID[0].id ? replacedPerson : person))
 				setNewName('')
 				setNewNumber('')
+				setSuccessMessage(`Successfully changed ${newName}'s contact number`)
 			})
 			.catch(error => console.error(error))
 		}
@@ -66,8 +70,17 @@ const App = () => {
           setPersons(persons.concat(addedPerson))
           setNewName('')
           setNewNumber('')
+		  setSuccessMessage(`Successfully added ${personObject.name}`)
+		  setTimeout(() => {
+			setSuccessMessage(null)
+		  }, 5000);
         })
-        .catch(error => console.error(error))
+        .catch(() => {
+			setErrorMessage(`Failed to add ${personObject.name}`)
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
+		})
       }
     }
   }
@@ -78,14 +91,24 @@ const App = () => {
 		.deleteName(personID)
 		.then(() => {
 			setPersons(persons.filter(person => person.id !== personID))
+			setSuccessMessage(`Sucessfully deleted ${persons.filter(person => person.id === personID)[0].name}`)
+			setTimeout(() => {
+				setSuccessMessage(null)
+			}, 5000);
 		})
-		.catch(error => console.error(error))
+		.catch(() => {
+			setErrorMessage(`failed to delete person ${personID}`)
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000);
+		})
 	}
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+	  <Notification error={errorMessage} success={successMessage}/>
       Filter shown with
       <Filter value={filterName} onChange={handleNameFilter}/>
       <h2>Add a new</h2>
